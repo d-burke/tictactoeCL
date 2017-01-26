ee1bdac82b6029ea9a7e818278b9d55f61e5b5dd
d-burke <omnidan@yahoo.com>
2017-01-26 10:19:27 -0800

    basic design pseudocode

---
+++ ticTacToeCommandLine.js
@@ -0,0 +1,12 @@
+//command line tic-tac-toe
+
+//start new game true
+
+//while game is not over
+//  display board
+//  prompt move
+//  verify win
+
+//display winner
+
+//prompt to start new game
